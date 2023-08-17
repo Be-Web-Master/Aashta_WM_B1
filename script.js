24c5340c6fef6d692310e980b0ede50f776e16fa
aasthashrivastav <aasthashrivastava2506>
2023-08-17 23:18:11 +0530

    task1

--- script.js
+++ script.js
@@ -1,24 +1,32 @@
-function array(num){
-        const optArr = []
-        for(let i=1;i <= num; i+=1){
-            optArr.push(i)
-        }
-        return optArr
+function array(num) {
+    const optArr = []
+    for (let i = 1; i <= num; i += 1) {
+        optArr.push(i)
     }
-let ages=array()
-let totalprice = 0
-function calculateTotalTicketPrice(ages){
-       if(ages<12){
-        totalprice = 5
-        console.log("$5")
-       }
-       if(ages>=12 && ages <= 17){
-        totalprice = 10
-        console.log("$10")
-       }
-       if(ages>=18 && ages <= 59){
-        totalprice = 10
-        console.log("$10")
-       }
-       
+    return optArr
 }
+let ages = array(20)
+console.log(ages)
+function calculateTotalTicketPrice(ages) {
+    let price, total = 0
+    for (let i = 0; i < ages.length; i += 1) {
+        if (ages < 12) {
+            price = 5
+            total += price
+        }
+        else if (ages >= 12 && ages <= 17) {
+            price = 10
+            total += price
+        }
+        else if (ages >= 18 && ages <= 59) {
+            price = 10
+            total += price
+        }
+        else {
+            price = 8
+            total += price
+        }
+    }
+    console.log(total)
+}
+calculateTotalTicketPrice(ages)
