7e9ceeba42375abd8df36c26251e77e0291b1c7d
aasthashrivastav <aasthashrivastava2506>
2023-08-23 11:29:23 +0530

    Form with Validation

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-let firstName=document.getElementById('firstName')
-let lastName=document.getElementById('lastName')
-let age=document.getElementById('age')
-let email=document.getElementById('email')
-let phone=document.getElementById('phone')
-let male=document.getElementById('male')
-let errorMsg=document.getElementById('error')
+const firstName = document.getElementById('firstName')
+const lastName = document.getElementById('lastName')
+const age = document.getElementById('age')
+const email = document.getElementById('email')
+const phone = document.getElementById('phone')
+const male = document.getElementById('male')
+const errorMsg = document.getElementById('error')
 
-const validateInput=(input)=>{
-  if(input === 'age' && age.value<18){
-    errorMsg.innerText='Age should be greater than 18'
-  }else if(input === 'phone' && phone.value<10){
-    errorMsg.innerText='phone number should be of 10 digit'
+const validateInput = (input) => {
+  if (input === 'age' && age.value < 18) {
+    errorMsg.innerText = 'Age should be greater than 18'
+  } else if (input === 'phone' && phone.value != 10) {
+    errorMsg.innerText = 'phone number should be of 10 digit'
   }
-  else{
-    errorMsg.innerText=""
+  else {
+    errorMsg.innerText = ""
   }
 }
-const formSubmit=(event)=>{
-   event.preventDefault()
-   console.log({
-    firstName:firstName.value,
-    lastName:lastName.value,
-    age:age.value,
-    email:email.value,
-    phone:phone.value,
-    gender:male.checked ? "male":"female"
-   })
+const handleFormSubmit = (event) => {
+  event.preventDefault()
+  console.log({
+    firstName: firstName.value,
+    lastName: lastName.value,
+    age: age.value,
+    email: email.value,
+    phone: phone.value,
+    gender: male.checked ? "male" : "female"
+  })
 }
 
